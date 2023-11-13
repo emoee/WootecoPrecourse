afbdd293480ffa65886e50a28ca37629d4036b5f
minsun <elinnane0@gmail.com>
2023-11-13 13:05:08 +0900

    ✨ feat(EVENT) : readMenu() 메소드에서 메뉴 개수 유효성 검사 구현

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -14,17 +14,37 @@ public class InputView {
     }
 
     public Order readMenu() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input = Console.readLine(); // 티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1
-        String[] menuList = input.split(",");
+        String[] menuList;
+        do {
+            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+            String input = Console.readLine().trim();
+            menuList = input.split(",");
+
+            if (calculateTotal(menuList) > 20) {
+                System.out.println("메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해주세요.");
+            }
+        } while (calculateTotal(menuList) > 20);
+
+        return setMenu(menuList);
+    }
+
+    private Order setMenu(String[] menuList){
         List<String> menuNames = new ArrayList<>();
         List<Integer> menuCounts = new ArrayList<>();
-        
+
         for (int i = 0; i < menuList.length; i++) {
             menuNames.add(menuList[i].split("-")[0]);
             menuCounts.add(Integer.parseInt(menuList[i].split("-")[1]));
         }
-
         return new Order(menuNames, menuCounts);
+    }
+
+    private int calculateTotal(String[] menuList) {
+        int total = 0;
+        for (String menu : menuList) {
+            int count = Integer.parseInt(menu.split("-")[1]);
+            total += count;
+        }
+        return total;
     }
 }
