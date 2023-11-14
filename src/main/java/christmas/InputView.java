676900345240b38884ff43c441d79559016a37d6
minsun <elinnane0@gmail.com>
2023-11-14 13:19:53 +0900

    ✨ feat(EVENT) : 주문 입력 시 유효성 검사 로직 추가

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -13,7 +13,6 @@ public class InputView {
             System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
             try {
                 visit_date = Integer.parseInt(Console.readLine());
-
                 if (visit_date >= 1 && visit_date <= 31) {
                     break; 
                 }
@@ -28,18 +27,39 @@ public class InputView {
     public Order readMenu() {
         String[] menuList;
 
-        do {
+        while (true) {
             System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
             String input = Console.readLine().trim();
             menuList = input.split(",");
 
-            if (calculateTotal(menuList) > 20) {
-                System.out.println("메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해주세요.");
+            try {
+                validateMenu(menuList);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } while (calculateTotal(menuList) > 20);
-
+        }
         return setMenu(menuList);
     }
+
+    private void validateMenu(String[] menuList) {
+        for (String menu : menuList) {
+            String menuName = menu.split("-")[0];
+            try {
+                Menu selectedMenu = Menu.valueOf(menuName);
+
+                if (!menu.matches("[가-힣]+-\\d{1,2}")) {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                }
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
+        if (calculateTotal(menuList) > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
+        }
+    }
+    
 
     private Order setMenu(String[] menuList){
         List<String> menuNames = new ArrayList<>();
@@ -56,8 +76,12 @@ public class InputView {
         int total = 0;
 
         for (String menu : menuList) {
-            int count = Integer.parseInt(menu.split("-")[1]);
-            total += count;
+            try {
+                int count = Integer.parseInt(menu.split("-")[1]);
+                total += count;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 주문 개수는 숫자로 입력해 주세요. 다시 입력해 주세요.");
+            }
         }
         return total;
     }
